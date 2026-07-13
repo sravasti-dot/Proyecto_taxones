59c2c02bd38b45bd2a4932d350ea4d079444d8c9
sravasti-dot <jimenezmateoangieq@gmail.com>
2026-07-12 23:10:51 -0600

    Versión modificada pruebas

--- pages/pruebas.py
+++ pages/pruebas.py
@@ -1,71 +1,68 @@
 import streamlit as st
-import io
-import sys
-from unittest.mock import MagicMock
-sys.modules["cv2"] = MagicMock()
 from ultralytics import YOLO
 from roboflow import Roboflow
 from PIL import Image
 
 st.set_page_config(page_title="Detector de taxones", layout="wide", page_icon="favicon.png")
 st.image("mi_portada.png", use_container_width=True)
+st.title("Sistema de monitoreo de macroinvertebrados")
+st.write("Detección de macroinvertebrados")
 rf = Roboflow(api_key="SiR5RA2UDruTVpmqk5jF")
 model_roboflow = rf.workspace("angie-oedt9").project("taxones").version(1).model
 
-st.title("Sistema de monitoreo de macroinvertebrados")
-
-if "imagen_final" not in st.session_state:
-    st.session_state.imagen_final = None
-
 tab1, tab2 = st.tabs(["Subir archivo", "Cámara dedicada"])
+archivo = None
+pixeles = None
+
+if "imagen_lista" not in st.session_state:
+     st.session_state.imagen_lista = False
 
 with tab1:
-   st.write("Sube una foto del taxón que deseas identificar")
-   archivo = st.file_uploader("Selecciona una imagen...", type=["jpg", "jpeg", "png"], key="uploader1")
-   
-   if archivo is not None:
-      imagen_final = Image.open(archivo).convert("RGB")
-      st.session_state.imagen_final = imagen_final
-      st.image(imagen_final, caption="Imagen cargada con éxito", width=90)
-    
+     st.write("Sube una imagen del taxón que deseas identificar")
+     archivo = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png", "heic"])
+     if archivo is not None:
+         st.session_state.imagen_lista = False  
+         try:
+             st.image(archivo, caption="Imagen capturada con éxito", width=90)
+             descarga = Image.open(archivo)
+             limpiar_imagen = descarga.convert('RGB')
+             limpiar_imagen.save("imagen.jpg")
+             st.session_state.imagen_lista = True
+         except Exception as e:
+             st.error (f"No pudimos procesar el archivo: {e}")  
+
+
 
 with tab2:
-   st.write("Usa la cámara dedicada de la web app para tomar una foto...")
-   foto_camara = st.camera_input("Tomar una foto desde este dispositivo...", key="camera1") 
-   
-   if foto_camara is not None:
-      imagen_final = Image.open(foto_camara).convert("RGB")
-      st.session_state.imagen_final = imagen_final
-      st.image(imagen_final, caption="Imagen capturada con éxito", width=90)
+     st.write("Usa la cámara dedicada de la web app para tomar una foto")
+     pixeles=st.camera_input("Tomar una foto desde este dispositivo...")
+     if pixeles is not None:
+         st.session_state.imagen_lista = False
+         try:
+             st.image(pixeles, caption="Imagen capturada con éxito", width=90)
+             foto_camara = Image.open(pixeles)
+             limpiar_imagen = foto_camara.convert('RGB')
+             limpiar_imagen.save("imagen.jpg")
+             st.session_state.imagen_lista = True
+         except Exception as e:
+             st.error (f"No pudimos procesar la foto: {e}")  
 
 
 if st.button("Realizar predicción"):
-  if st.session_state.imagen_final is not None:
-    with st.spinner("Realizando predicción..."):
-        try:
-          ruta_temporal = "/tmp/imagen.jpg"
-          st.session_state.imagen_final.save(ruta_temporal, format="JPEG")
-          
-          prediccion = model_roboflow.predict("ruta_temporal", confidence=70, overlap=30)
-          
-          resultados_buffer = io.BytesIO()
+     if st.session_state.imagen_lista:
+       with st.spinner("Realizando predicción..."):
+          prediccion = model_roboflow.predict("imagen.jpg", confidence=70, overlap=30)
           prediccion.save("resultado.jpg")
-          resultados_buffer.seek(0)
-          
-          st.image(resultados_buffer, caption="Resultado", width=400)
-          
           datos = prediccion.json()
-          st.subheader("Taxones detectados en la muestra")
-    
+          st.image("resultado.jpg", caption="Resultado de la predicción", width=400)
+          st.subheader("Taxones detectados en la muestra:")
           if "predictions" in datos and len(datos["predictions"])>0:
-                  for taxon in datos["predictions"]:
-                      nombre_taxon= taxon["class"]
-                      certeza = taxon["confidence"]*100
-                      st.write(f"**{nombre_taxon}** con una certeza de **{certeza:.2f}%**")
+             for taxon in datos["predictions"]:
+                nombre_taxon= taxon["class"]
+                certeza = taxon["confidence"]*100
+                st.write(f"**{nombre_taxon}** con una certeza de **{certeza:.2f}%**")
           else:
-                 st.write("No se detectaron taxones en la imagen.")
-        except Exception as e:
-          st.error(f"Hubo un error al generar la imagen. Por favor, intenta nuevamente. {e}")      
-        
-  else:
-       st.warning("Por favor, sube una imagen o toma una foto para realizar la predicción.")
+            st.write("No se detectaron taxones en la imagen.")
+
+                 
+
