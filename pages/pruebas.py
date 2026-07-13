f5be08c5e82f6eb2f5cbfc56a93daf36eaf8f37a
sravasti-dot <jimenezmateoangieq@gmail.com>
2026-07-12 19:20:18 -0600

    Agregar tabs y lógica de cámara

--- pages/pruebas.py
+++ pages/pruebas.py
@@ -5,37 +5,39 @@ sys.modules["cv2"] = MagicMock()
 from ultralytics import YOLO
 from roboflow import Roboflow
 from PIL import Image
+import os
 st.set_page_config(page_title="Detector de taxones", layout="wide", page_icon="favicon.png")
 st.image("mi_portada.png", use_container_width=True)
-st.title("Sistema de monitoreo de macroinvertebrados")
-st.write("Sube una foto del taxón que deseas identificar")
 rf = Roboflow(api_key="SiR5RA2UDruTVpmqk5jF")
 model_roboflow = rf.workspace("angie-oedt9").project("taxones").version(1).model
-espacio= st.empty()
-archivo = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png"])
-activar_camara = st.checkbox("Encender cámara")
-foto_camara = None
-imagen_lista = False
-if archivo is not None:
-   imagen = Image.open(archivo)
-   with espacio:
-      espacio=st.columns([1, 7])
-   st.image(imagen, caption="Imagen cargada con éxito", width=90)
-   if imagen.mode == "RGBA":
-        imagen = imagen.convert("RGB")
+st.title("Sistema de monitoreo de macroinvertebrados")
+tab1, tab2 = st.tabs(["Subir archivo", "Cámara dedicada"])
+imagen_final = None
+imagen_lista= False
+
+with tab1:
+   st.write("Sube una foto del taxón que deseas identificar")
+   archivo = st.file_uploader("Selecciona una imagen...", type=["jpg", "jpeg", "png"], key="uploader1")
+   if archivo is not None:
+      imagen_final = Image.open(archivo)
+      st.image(imagen_final, caption="Imagen cargada con éxito", width=90)
+      if imagen_final.mode == "RGBA":
+        imagen = imagen_final.convert("RGB")
         ruta_temporal = "temp_image.jpg"
         imagen.save(ruta_temporal)
-   imagen.save("imagen.jpg")
-   imagen_lista= True
-if activar_camara:
-   foto_camara=st.camera_input("Tomar una foto desde este dispositivo")
+      imagen_final.save("imagen.jpg")
+      imagen_lista= True
+
+with tab2:
+   st.write("Usa la cámara dedicada de la web app para tomar una foto...")
+   foto_camara = st.camera_input("Tomar una foto desde este dispositivo...", key="camera1") 
    if foto_camara is not None:
-       imagen = Image.open(foto_camara)
-       st.image(imagen, caption="Imagen capturada con éxito", width=90)
-       imagen.save("imagen.jpg")
-       imagen_lista= True
+      imagen_final = Image.open(foto_camara)
+      imagen_final.save("imagen.jpg")
+      st.image(imagen_final, caption="Imagen capturada con éxito", width=90)
+      imagen_lista=True
 if st.button("Realizar predicción"):
-   if imagen_lista:
+    if imagen_lista:
        with st.spinner("Realizando predicción..."):
           prediccion = model_roboflow.predict("imagen.jpg", confidence=70, overlap=30)
           prediccion.save("resultado.jpg")
@@ -49,3 +51,5 @@ if st.button("Realizar predicción"):
                 st.write(f"**{nombre_taxon}** con una certeza de **{certeza:.2f}%**")
           else:
             st.write("No se detectaron taxones en la imagen.")
+    else:
+        st.warning("Por favor, sube una imagen o toma una foto para realizar la predicción.")
