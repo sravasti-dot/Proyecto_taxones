83bb5944d2cfad55db699eb4dae2f0b678de2d87
sravasti-dot <jimenezmateoangieq@gmail.com>
2026-07-12 19:58:12 -0600

    Pruebas

--- pages/pruebas.py
+++ pages/pruebas.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import io
 import sys
 from unittest.mock import MagicMock
 sys.modules["cv2"] = MagicMock()
@@ -11,8 +12,10 @@ st.image("mi_portada.png", use_container_width=True)
 rf = Roboflow(api_key="SiR5RA2UDruTVpmqk5jF")
 model_roboflow = rf.workspace("angie-oedt9").project("taxones").version(1).model
 st.title("Sistema de monitoreo de macroinvertebrados")
+if "imagen_final" not in st.session_state:
+    st.session_state.imagen_final = None
 tab1, tab2 = st.tabs(["Subir archivo", "Cámara dedicada"])
-imagen_final = None
+imagen_final = st.session_state.imagen_final
 imagen_lista= False
 
 with tab1:
@@ -20,6 +23,7 @@ with tab1:
    archivo = st.file_uploader("Selecciona una imagen...", type=["jpg", "jpeg", "png"], key="uploader1")
    if archivo is not None:
       imagen_final = Image.open(archivo)
+      st.session_state.imagen_final = imagen_final
       st.image(imagen_final, caption="Imagen cargada con éxito", width=90)
       if imagen_final.mode == "RGBA":
         imagen = imagen_final.convert("RGB")
@@ -33,15 +37,24 @@ with tab2:
    foto_camara = st.camera_input("Tomar una foto desde este dispositivo...", key="camera1") 
    if foto_camara is not None:
       imagen_final = Image.open(foto_camara)
+      st.session_state.imagen_final = imagen_final
       imagen_final.save("imagen.jpg")
       st.image(imagen_final, caption="Imagen capturada con éxito", width=90)
       imagen_lista=True
 if st.button("Realizar predicción"):
+  if st.session_state.imagen_final is not None:
     if imagen_lista:
        with st.spinner("Realizando predicción..."):
+          img_buffer = io.BytesIO()
+          st.session_state.imagen_final.save(img_buffer, format="JPEG")
+          img_buffer.seek(0)
           prediccion = model_roboflow.predict("imagen.jpg", confidence=70, overlap=30)
           prediccion.save("resultado.jpg")
           if os.path.exists("resultado.jpg"):
+              resultados_buffer = io.BytesIO()
+              prediccion.save(resultados_buffer)
+              resultados_buffer.seek(0)
+              st.image(resultados_buffer, caption="Resultado", width=400)
               datos = prediccion.json()
               st.image("resultado.jpg", caption="Resultado de la predicción", width=400)
     
