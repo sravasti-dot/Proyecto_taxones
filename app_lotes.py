bbefc13d0623afaaf96d3bfeff386396ecda0bb3
sravasti-dot <jimenezmateoangieq@gmail.com>
2026-08-07 00:03:20 -0600

    nuevo

--- app_lotes.py
+++ app_lotes.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from roboflow import Roboflow
 from PIL import Image
+import hashlib
 
 st.set_page_config(page_title="Detector de taxones", layout="wide", page_icon="favicon.png")
 st.image("mi_portada.png", use_container_width=True)
@@ -16,20 +17,46 @@ pixeles = None
 if "imagen_lista" not in st.session_state:
      st.session_state.imagen_lista = False
 
+if "hashes_procesados" not in st.session_state:
+    st.session_state.hashes_procesados = set()     
+
 with tab1:
      st.write("Sube una imagen del taxón que deseas identificar")
-     archivo = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png", "heic"])
-     if archivo is not None:
-         st.session_state.imagen_lista = False  
+     archivo = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png", "heic"], accept_multiple_files=True)
+     if st.button("Borrar y empezar de nuevo"):
+              st.session_state.hashes_procesados.clear()
+              st.session_state.imagen_lista = False
+              st.rerun()
+     if archivo is not None and len(archivo) <= 5:
+         st.session_state.imagen_lista = False 
+
          try:
-             st.image(archivo, caption="Imagen capturada con éxito", width=90)
-             descarga = Image.open(archivo)
-             limpiar_imagen = descarga.convert('RGB')
-             limpiar_imagen.save("imagen.jpg")
-             st.session_state.imagen_lista = True
+          hashes_actuales = set()
+
+          for file in archivo:
+             
+             bytes_archivo = file.read()
+             hash_md5 = hashlib.md5(bytes_archivo).hexdigest()
+
+             if hash_md5 in st.session_state.hashes_procesados:
+                st.warning(f"⚠️ El archivo '{file.name}' ya fue subido anteriormente.")
+
+             elif hash_md5 in hashes_actuales:
+                st.warning(f"⚠️ Estás intentando subir '{file.name}' por duplicado en este grupo.")
+             else:
+                hashes_actuales.add(hash_md5)
+                st.session_state.hashes_procesados.add(hash_md5)
+
+                st.image(file, caption="Se ha capturado con éxito", width=90)
+                descarga = Image.open(file)
+                limpiar_imagen = descarga.convert('RGB')
+                limpiar_imagen.save("imagen.jpg")
+                st.session_state.imagen_lista = True
+         
          except Exception as e:
              st.error (f"No pudimos procesar el archivo: {e}")  
-
+     else:
+        st.error("Por favor, sube un máximo de 5 imágenes.")  
 
 
 with tab2:
