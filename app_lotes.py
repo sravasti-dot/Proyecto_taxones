ab859d8bd3087e53fed1ee9dcfd220210c72e133
sravasti-dot <jimenezmateoangieq@gmail.com>
2026-08-07 23:39:23 -0600

    nose

--- app_lotes.py
+++ app_lotes.py
@@ -10,54 +10,47 @@ st.write("Detección de macroinvertebrados")
 rf = Roboflow(api_key="SiR5RA2UDruTVpmqk5jF")
 model_roboflow = rf.workspace("angie-oedt9").project("taxones").version(1).model
 
+
 tab1, tab2 = st.tabs(["Subir archivo", "Cámara dedicada"])
-archivo = None
+archivero = None
 pixeles = None
+duplicado = False
 
 if "imagen_lista" not in st.session_state:
      st.session_state.imagen_lista = False
 
-if "hashes_procesados" not in st.session_state:
-    st.session_state.hashes_procesados = set()     
+if "imagenes_procesadas" not in st.session_state:
+    st.session_state.imagenes_procesadas = set()          
 
 with tab1:
      st.write("Sube una imagen del taxón que deseas identificar")
-     archivo = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png", "heic"], accept_multiple_files=True)
-     if st.button("Borrar y empezar de nuevo"):
-              st.session_state.hashes_procesados.clear()
-              st.session_state.imagen_lista = False
-              st.rerun()
-     if archivo is not None and len(archivo) <= 5:
-         st.session_state.imagen_lista = False 
+     archivero = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png", "heic"], accept_multiple_files=True)
 
+     if archivero is not None and len(archivero) <= 2:
+         st.session_state.imagen_lista = False  
          try:
-          hashes_actuales = set()
+           for file in archivero:  
+             leer_file = file.getvalue()
+             huella = hashlib.md5(leer_file).hexdigest()
 
-          for file in archivo:
-             
-             bytes_archivo = file.read()
-             hash_md5 = hashlib.md5(bytes_archivo).hexdigest()
-
-             if hash_md5 in st.session_state.hashes_procesados:
-                st.warning(f"⚠️ El archivo '{file.name}' ya fue subido anteriormente.")
-
-             elif hash_md5 in hashes_actuales:
-                st.warning(f"⚠️ Estás intentando subir '{file.name}' por duplicado en este grupo.")
+             if huella in st.session_state.imagenes_procesadas:
+                 st.warning(f"La imagen {file.name} ya ha sido procesada anteriormente.")
+                 duplicado = True
+            
+             if duplicado:
+                st.stop()
              else:
-                hashes_actuales.add(hash_md5)
-                st.session_state.hashes_procesados.add(hash_md5)
+                 st.session_state.imagenes_procesadas.add(huella)
+                 st.image(file, caption=f"Imagen '{file.name}' capturada con éxito", width=90)
 
-                st.image(file, caption="Se ha capturado con éxito", width=90)
-                descarga = Image.open(file)
-                limpiar_imagen = descarga.convert('RGB')
-                limpiar_imagen.save("imagen.jpg")
-                st.session_state.imagen_lista = True
-         
+                 descarga = Image.open(file)
+                 limpiar_imagen = descarga.convert('RGB')
+                 limpiar_imagen.save("imagen.jpg")
+                 st.session_state.imagen_lista = True
          except Exception as e:
-             st.error (f"No pudimos procesar el archivo: {e}")  
+             st.error (f"No pudimos procesar el archivo: {e}") 
      else:
-        st.error("Por favor, sube un máximo de 5 imágenes.")  
-
+         st.error("Por favor, sube un máximo de 2 imágenes.")      
 
 with tab2:
      st.write("Usa la cámara dedicada de la web app para tomar una foto")
@@ -72,7 +65,6 @@ with tab2:
              st.session_state.imagen_lista = True
          except Exception as e:
              st.error (f"No pudimos procesar la foto: {e}")  
-
 
 if st.button("Realizar predicción"):
      if st.session_state.imagen_lista:
